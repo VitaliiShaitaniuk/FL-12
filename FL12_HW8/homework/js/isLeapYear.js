eff0160845d1daac18f2a65cc248583aa552c784
Vitalii Shaitaniuk <44817200+VitaliiShaitaniuk@users.noreply.github.com>
2019-12-31 22:18:16 +0200

    Update is leap year
    is leap year finished. Endlich!

--- FL12_HW8/homework/js/isLeapYear.js
+++ FL12_HW8/homework/js/isLeapYear.js
@@ -1,3 +1,14 @@
-function isLeapYear(){
-    
+function isLeapYear(input){
+    let year = new Date(input).getFullYear();
+    if (isNaN(year)) {
+        return 'Invalid Date.';
+    }
+    if(year % 4 !==0 && year % 400 !== 0){
+        return year + " is not a leap year";
+    }else if(year % 100 !== 0){
+        return year + " is a leap year";
+    }else{
+        return year + " is a leap year";
+    }
 }
+isLeapYear('2020-01-01 00:00:00'); 
