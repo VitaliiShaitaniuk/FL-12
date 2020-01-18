a14b93cb74572bf722db397516815fddb9d1b25c
Vitalii Shaitaniuk <44817200+VitaliiShaitaniuk@users.noreply.github.com>
2020-01-19 01:09:35 +0200

    HW10
    structure and function Fighter

---
+++ FL12_HW10/homework/js/fighters_game.js
@@ -0,0 +1,10 @@
+function Fighter(name, damage, hp, strength, agility){
+    this.name = name;
+    this.damage = damage;
+    this.hp = hp;
+    this.strength = strength;
+    this.agility = agility;
+    getName
+}   
+
+const myFighter = new Fighter({name: 'Maximus', damage: 25, hp: 100, strength: 30, agility: 25}); 
