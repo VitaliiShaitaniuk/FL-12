f52c7528f1974d6cefc30ad6929fd4a96167a799
Vitalii Shaitaniuk <44817200+VitaliiShaitaniuk@users.noreply.github.com>
2020-01-19 22:58:37 +0200

    HW10
    Constructor is better

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -1,10 +1,14 @@
-function Fighter(name, damage, hp, strength, agility){
-    this.name = name;
-    this.damage = damage;
-    this.hp = hp;
-    this.strength = strength;
-    this.agility = agility;
-    getName
-}   
-
+class Fighter {
+    constructor({name, damage, hp, strength, agility}) {
+        this._name = name;
+        this._damage = damage;
+        this._hp = hp;
+        this._strength = strength;
+        this._agility = agility;
+        
+    }
+    
+}
 const myFighter = new Fighter({name: 'Maximus', damage: 25, hp: 100, strength: 30, agility: 25}); 
+let name = myFighter.name;
+console.log(name); 
